0c95e842adcd5a6af9eaf1ff82bde9cb0fff4b42
William Melody <hi@williammelody.com>
2016-01-24 22:31:42 -0800

    Use /tmp directory for temp file generated with `mktemp`.
    Moving to `mktemp` with files in /tmp makes it possible to remove the
    local test/tmp directory.

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -4,10 +4,15 @@ setup() {
   # The location of the `hosts` script being tested.
   export _HOSTS="${BATS_TEST_DIRNAME}/../hosts"
 
-  export HOSTS_PATH="${BATS_TEST_DIRNAME}/tmp/hosts"
-  cp "${BATS_TEST_DIRNAME}/fixtures/hosts" "${BATS_TEST_DIRNAME}/tmp/hosts"
+  export HOSTS_PATH="$(mktemp /tmp/hosts_test.XXXXXX)" || exit 1
+  cat "${BATS_TEST_DIRNAME}/fixtures/hosts" > "${HOSTS_PATH}"
 }
 
 teardown() {
-  rm "${BATS_TEST_DIRNAME}/tmp/hosts"
+  if [[ -n "${HOSTS_PATH}" ]] &&
+     [[ -e "${HOSTS_PATH}" ]] &&
+     [[ "${HOSTS_PATH}" =~ ^/tmp ]]
+  then
+    rm "${HOSTS_PATH}"
+  fi
 }
